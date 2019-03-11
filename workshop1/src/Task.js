b5284416bfe266f5b010de4098ab7ccef02fb19d
Rob Baile <robbaile12@outlook.com>
2019-03-11 15:20:54 +0000

    updated task.js

--- workshop1/src/Task.js
+++ workshop1/src/Task.js
@@ -2,12 +2,27 @@ import React, { Component } from 'react';
 import './Task.css';
 
 class Task extends Component {
+  constructor() {
+    super()
+    this.state = {
+      isAccepted: false,
+    };
+  }
+
+  handleAcceptTask = () => {
+    this.setState({
+      isAccepted: !this.state.isAccepted,
+    });
+    
+  };
 
   render() {
     return (
-    <div className="task">
+    <div className={`task__${this.state.isAccepted}`}>
         <h2 className="task__title">Task: {this.props.title}</h2>
         <p className="task__body">Description: {this.props.description}</p>
+        <p>Check to accept task</p><input type="checkbox" onClick={this.handleAcceptTask}></input>
+        {this.state.isAccepted && <p className="p-accepted">This task has been accepted</p>}
     </div>
     );
   }
