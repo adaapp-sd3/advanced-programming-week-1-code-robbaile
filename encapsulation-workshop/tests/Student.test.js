35386da4639db2cb0050df418dc2c72ed23c3599
Rob Baile <robbaile12@outlook.com>
2019-03-12 12:08:58 +0000

    written failing tests for student.js

--- encapsulation-workshop/tests/Student.test.js
+++ encapsulation-workshop/tests/Student.test.js
@@ -9,8 +9,16 @@ it('should run and pass the test', () => {
 });
 
 describe("Student class tests", () => {
-    let newStudent = new Student();
+    let newStudent = new Student("Rob Baile", 11, "11C");
     it('should create a new year group', () => {
         expect(newStudent).to.be.an.instanceOf(Student);
+        expect(newStudent.name).to.equal("Rob Baile");
+        expect(newStudent.year).to.equal(11);
+        expect(newStudent.form).to.equal("11C");
     });
+    it('should return name, yearGroup, formGroup', () => {
+        expect(newStudent.getName()).to.equal("Rob Baile");
+        expect(newStudent.getYearGroup()).to.equal(11);
+        expect(newStudent.getFormGroup()).to.equal("11C");
+    })
 });
