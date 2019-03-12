e33a263208c3bb8c6a042ca0f635d2a2f192a42c
Rob Baile <robbaile12@outlook.com>
2019-03-12 12:11:21 +0000

    written student.js to get code to pass

--- encapsulation-workshop/models/Student.js
+++ encapsulation-workshop/models/Student.js
@@ -1,6 +1,20 @@
 class Student {
-    constructor() {
+    constructor(name, year, form) {
+        this.name = name;
+        this.year = year;
+        this.form = form;
+    }
 
+    getName() {
+        return this.name;
+    };
+
+    getYearGroup() {
+        return this.year;
+    };
+
+    getFormGroup() {
+        return this.form;
     }
 }
 
