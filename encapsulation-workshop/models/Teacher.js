c4ae4923356c91e54d24d6c9e1e5b24959632c5c
Rob Baile <robbaile12@outlook.com>
2019-03-12 14:25:04 +0000

    written the code for the teacher.js class

--- encapsulation-workshop/models/Teacher.js
+++ encapsulation-workshop/models/Teacher.js
@@ -5,6 +5,26 @@ class Teacher {
         this.subject = subject;
         this.initials = initials;
     };
+
+    getName() {
+        return this.name;
+    };
+
+    getYearsTeaching() {
+        return this.yearsTeaching;
+    };
+
+    getSubject() {
+        return this.subject;
+    };
+
+    getInitials() {
+        return this.initials;
+    };
+
+    setYearsTeaching(newYearsTeaching) {
+        this.yearsTeaching = newYearsTeaching;
+    };
 };
 
 module.exports = Teacher;
